774f09e3a767c4c2fd99a3548e901438b20909cc
Lledune <lucien.ledune@student.uclouvain.be>
2018-03-31 03:38:53 +0200

    ;jgyut

--- DM.R
+++ DM.R
@@ -143,6 +143,9 @@ modMatrix<- function(dice,trap,circle){ #Must be used on a matrix full of 0's
       
     }
     for(j in trapList2){
+      if(j==11){
+        
+      }else{
       #Modifying trap 2 probabilities
       M[j-3, 1] = M[j-3, 1] + M[j-3, j]
       M[j-3, j] = 0
@@ -155,7 +158,7 @@ modMatrix<- function(dice,trap,circle){ #Must be used on a matrix full of 0's
       
       M[j, 1] = M[j, 1] + M[j, j]
       M[j, j] = 0
-      
+      }
     }
     M = round(M,2)
     
@@ -170,40 +173,43 @@ trap = c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0) #trap list
 # Choice of policy ###########################
 ##############################################
 
-circle = 1 #Can't go past case 15 (set to 1 if it is possible to do so)
 
 
-#Init matrix for each dice
-safeM = modMatrix(0, trap, circle)
-normM = modMatrix(1, trap, circle)
-riskM = modMatrix(2, trap, circle)
 
-
-getProb <- function(dice, start){ #Getting prob
-  if(dice == 0){
-    return(safeM[start,])
-  }else if(dice == 1){
-    return(normM[start,])
-  }else if(dice == 2){
-    return(riskM[start,])
-  }
-}
-
-Rsa = matrix(1,15,1) #cost matrix
-Rsa[15, ] = 0 #cost at 15 is null
-policy = matrix(0,15,1) #Init policy
-VkT = Rsa
-
-Vksa = NULL
-for(k in 1:1000){ #for k iterations (convergence)
-  for(s in 1:15){ #for all states
-    for(a in 0:2){ #for every possible action
-      Vksa[a+1] = Rsa[s] + getProb(a, s) %*% VkT
+VKF <- function(trap, circle){
+  getProb <- function(dice, start){ #Getting prob
+    if(dice == 0){
+      return(safeM[start,])
+    }else if(dice == 1){
+      return(normM[start,])
+    }else if(dice == 2){
+      return(riskM[start,])
     }
-    VkT[s] = min(Vksa) #Getting min
-    policy[s] = which.min(Vksa)-1
   }
+  #Init matrix for each dice
+  safeM = modMatrix(0, trap, circle)
+  normM = modMatrix(1, trap, circle)
+  riskM = modMatrix(2, trap, circle)
+  
+  Rsa = matrix(1,15,1) #cost matrix
+  Rsa[15, ] = 0 #cost at 15 is null
+  policy = matrix(0,15,1) #Init policy
+  VkT = Rsa
+  
+  Vksa = NULL
+  for(k in 1:1000){ #for k iterations (convergence)
+    for(s in 1:15){ #for all states
+      for(a in 0:2){ #for every possible action
+        Vksa[a+1] = Rsa[s] + getProb(a, s) %*% VkT
+      }
+      VkT[s] = min(Vksa) #Getting min
+      policy[s] = which.min(Vksa)-1
+    }
+  }
+  return(list(VkT, policy))
 }
+
+listt = VKF(trap, 1)
 
 
 
